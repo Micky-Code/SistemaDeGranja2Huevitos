bb6620e40114176c603b642ed04f0146e171d9da
juancruz0346-design <juancruz0346@ejemplo.com>
2026-09-18 14:04:36 -0500

    Actualización en frontend_infraestructura_produccion

--- src/main/java/com/granja/dos/huevitos/controller/PageController.java
+++ src/main/java/com/granja/dos/huevitos/controller/PageController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.security.core.Authentication;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.security.Principal;
 
 @Controller
 public class PageController {
